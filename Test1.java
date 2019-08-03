560319b8c70543bc1fbcdeb0ddc5367d9ceeb67a
Halo-Mars <52438383+Halo-Mars@users.noreply.github.com>
2019-08-03 09:51:03 +0800

    优化代码
    一点点的修改

--- Test1.java
+++ Test1.java
@@ -2,6 +2,7 @@ package com.idea;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("my first GitHub project!");
+       // System.out.println("my first GitHub project!");
+	System.out.println("my first GitHub project! iloveyou");
     }
 }
